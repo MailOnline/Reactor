4f7fa11e4c30c2c1672c3f290babb4fa1798f7f9
Rui Peres <rui.peres@mailonline.co.uk>
2016-03-11 19:16:37 +0000

    Added the Resource

---
+++ Reactor/Core/Resource.swift
@@ -0,0 +1,104 @@
+//
+//  Resource.swift
+//  Reactor
+//
+//  Created by Rui Peres on 11/03/2016.
+//  Copyright © 2016 Mail Online. All rights reserved.
+//
+
+import Foundation
+
+public typealias Header = [String: String]
+public typealias Query = [String: String]
+
+// Stolen from chriseidhof/github-issues 😅
+public struct Resource: Equatable, CustomStringConvertible {
+    
+    public let path: String
+    public let method: Method
+    public let headers: Header
+    public let body: NSData?
+    public let query: Query
+    
+    public init(path: String, method: Method, body: NSData? = nil, headers: Header = [:], query: Query = [:]) {
+        self.path = path
+        self.method = method
+        self.body = body
+        self.headers = headers
+        self.query = query
+    }
+    
+    public var description: String {
+        return "Path:\(path)\nMethod:\(method.rawValue)\nHeaders:\(headers)"
+    }
+}
+
+public func == (lhs: Resource, rhs: Resource) -> Bool {
+    
+    var equalBody = false
+    
+    switch (lhs.body, rhs.body) {
+    case (nil,nil): equalBody = true
+    case (nil,_?): equalBody = false
+    case (_?,nil): equalBody = false
+    case (let l?,let r?): equalBody = l.isEqualToData(r)
+    }
+    
+    return (lhs.path == rhs.path && lhs.method == rhs.method && equalBody)
+}
+
+public enum Method: String {
+    case OPTIONS = "OPTIONS"
+    case GET = "GET"
+    case HEAD = "HEAD"
+    case POST = "POST"
+    case PUT = "PUT"
+    case PATCH = "PATCH"
+    case DELETE = "DELETE"
+    case TRACE = "TRACE"
+    case CONNECT = "CONNECT"
+}
+
+extension Resource {
+    
+    public func toRequest(baseURL: NSURL) -> NSURLRequest {
+        
+        let components = NSURLComponents(URL: baseURL, resolvingAgainstBaseURL: false)
+        
+        let queryString = createQueryString(query)
+        if queryString != "" {
+            components?.query = queryString
+        }
+        components?.path = path
+        
+        let finalURL = components?.URL ?? baseURL
+        let request = NSMutableURLRequest(URL: finalURL)
+        
+        request.HTTPBody = body
+        request.allHTTPHeaderFields = headers
+        request.HTTPMethod = method.rawValue
+        
+        return request
+    }
+    
+    public func addHeader(value: String, key: String) -> Resource {
+        
+        var headers = self.headers
+        headers[key] = value
+        
+        return Resource(path: path, method: method, body: body, headers: headers, query: query)
+    }
+}
+
+private func createQueryString(query: Query) -> String {
+    
+    guard query.isEmpty == false else { return "" }
+    
+    var queryString = ""
+    
+    for key in query.keys {
+        queryString.appendContentsOf("\(key)=\(query[key]!)&")
+    }
+    
+    return String(queryString.characters.dropLast())
+}
---
+++ ReactorTests/Tests/ResourceTests.swift
@@ -0,0 +1,72 @@
+//
+//  ResourceTests.swift
+//  Reactor
+//
+//  Created by Rui Peres on 11/03/2016.
+//  Copyright © 2016 Mail Online. All rights reserved.
+//
+
+import XCTest
+@testable import Reactor
+
+class ResourceTests: XCTestCase {
+
+    func testEqualityForNilBody() {
+        let resource1 = Resource(path: "/path/1", method: .GET)
+        let resource2 = Resource(path: "/path/1", method: .GET)
+        
+        XCTAssertEqual(resource1, resource2)
+    }
+    
+    func testEqualityForNonNilBody() {
+        let resource1 = Resource(path: "/path/1", method: .GET)
+        let resource2 = Resource(path: "/path/1", method: .GET)
+        
+        XCTAssertEqual(resource1, resource2)
+    }
+    
+    func testInequalityForDifferentBody() {
+        let resource1 = Resource(path: "/path/1", method: .GET)
+        let resource2 = Resource(path: "/path/1", method: .GET, body: NSData())
+        
+        XCTAssertNotEqual(resource1, resource2)
+        XCTAssertNotEqual(resource2, resource1)
+    }
+    
+    func testDescription() {
+        let resource = Resource(path: "/path/", method: .GET)
+        
+        XCTAssertEqual(resource.description, "Path:/path/\nMethod:GET\nHeaders:[:]")
+    }
+    
+    func testRequestConversion() {
+        let body = "Hello World".dataUsingEncoding(NSUTF8StringEncoding)
+        let headers = ["key" : "value"]
+        let baseURL = NSURL(string: "http://dailyMail.api")
+        
+        let resource = Resource(path: "/path/1", method: .GET, body: body, headers: headers)
+        let request = resource.toRequest(baseURL!)
+        
+        XCTAssertEqual(request.URL!, baseURL?.URLByAppendingPathComponent(resource.path))
+        XCTAssertEqual(request.HTTPMethod!, resource.method.rawValue)
+        XCTAssertEqual(request.allHTTPHeaderFields!, resource.headers)
+        XCTAssertEqual(request.allHTTPHeaderFields!, resource.headers)
+        XCTAssertEqual(request.HTTPBody!, resource.body!)
+    }
+    
+    func testAddingNewHeaderWithEmptyHeaders() {
+        let resource = Resource(path: "/path/1", method: .GET)
+        let newResource = resource.addHeader("value", key: "key")
+        
+        XCTAssertEqual(newResource.headers.keys.count, 1)
+        XCTAssertEqual(newResource.headers["key"], "value")
+    }
+    
+    func testAddingNewHeaderWithHeaders() {
+        let resource = Resource(path: "/path/1", method: .GET, body: nil, headers: ["value1" : "key1"])
+        let newResource = resource.addHeader("value1", key: "key1")
+        
+        XCTAssertEqual(newResource.headers.keys.count, 2)
+        XCTAssertEqual(newResource.headers["key1"], "value1")
+    }
+}
