fa129ebed1c0e016af39ad36c1fb4f29bff1a695
Rui Peres <rui.peres@mailonline.co.uk>
2016-03-14 16:58:59 +0000

    Initial Reactor draft

---
+++ Reactor/Reactor/Reactor.swift
@@ -0,0 +1,56 @@
+//
+//  Reactor.swift
+//  Reactor
+//
+//  Created by Rui Peres on 14/03/2016.
+//  Copyright © 2016 Mail Online. All rights reserved.
+//
+
+import ReactiveCocoa
+
+struct Information<T: InDiskElementsPersistence where T.Model: Mappable> {
+    
+    let connection: Connection
+    let persister: T
+    let experitationInMinutes: NSTimeInterval
+    let parser: NSData -> SignalProducer<[T.Model], Error> = parse
+}
+
+struct Reactor<T: InDiskElementsPersistence where T.Model: Mappable> {
+
+    typealias Model = T.Model
+
+    let information: Information<T>
+    
+    init(information: Information<T>) {
+        self.information = information
+    }
+    
+    func fetch(resource: Resource) -> SignalProducer<[Model], Error> {
+        
+        let experitationTime = information.experitationInMinutes
+        let persist = information.persister
+        
+        let experitationHandler: Bool -> SignalProducer <[Model], Error> = { hasExpired in
+        
+            if hasExpired {
+                return self.fetchFromNetwork(resource)
+            }
+            else {
+                return persist.load()
+            }
+        }
+        
+        return persist.hasPersistenceExpired(expirationInMinutes: experitationTime)
+            .flatMapError { _ in SignalProducer(error: Error.Persistence("")) }
+            .flatMapLatest(experitationHandler)
+    }
+    
+    func fetchFromNetwork(resource: Resource) -> SignalProducer<[Model], Error> {
+        
+        let connection = information.connection
+        let parser = information.parser
+
+        return connection.makeRequest(resource).map { $0.0 }.flatMapLatest(parser)
+    }
+}
